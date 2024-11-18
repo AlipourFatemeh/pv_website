4e6f0c16e68e3fe806ff36abad3e58c88e78c605
Fatemeh_Aalipour <117110216+AlipourFatemeh@users.noreply.github.com>
2024-11-18 14:22:40 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings:["Frontend Developer" , "computer engineer" ,"web programmer" , " I live in Shahrekord"],
+    typeSpeed : 100,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop:true
+});
